1df2e4320b601188886829fea2c1bc6042751655
Fraida Fund <ffund@nyu.edu>
2021-01-11 20:33:35 -0500

    Update dir-install.sh

--- dir-install.sh
+++ dir-install.sh
@@ -18,6 +18,7 @@ finger1=$(sudo cat /var/lib/tor/keys/authority_certificate  | grep fingerprint |
 finger2=$(sudo cat /var/lib/tor/fingerprint | cut -f 2 -d ' ')
 
 HOSTNAME=$(hostname -s)
+ADDRESS=$(hostname -I | tr " " "\n" | grep "10.10")
 
 sudo bash -c "cat >/etc/tor/torrc <<EOL
 TestingTorNetwork 1
@@ -33,14 +34,14 @@ Log debug file /var/log/tor/debug.log
 ProtocolWarnings 1
 SafeLogging 0
 DisableDebuggerAttachment 0
-DirAuthority $HOSTNAME orport=5000 no-v2 hs v3ident=$finger1 192.168.1.4:7000 $finger2
+DirAuthority $HOSTNAME orport=5000 no-v2 hs v3ident=$finger1 $ADDRESS:7000 $finger2
 SocksPort 0
 OrPort 5000
 ControlPort 9051
-Address 192.168.1.4
+Address $ADDRESS
 DirPort 7000
 # An exit policy that allows exiting to IPv4 LAN
-ExitPolicy accept 192.168.0.0/16:*
+ExitPolicy accept 10.10.0.0/16:*
 AuthoritativeDirectory 1
 V3AuthoritativeDirectory 1
 ContactInfo auth0@test.test
@@ -50,44 +51,18 @@ TestingV3AuthInitialVoteDelay 20
 TestingV3AuthInitialDistDelay 20
 EOL"
 
-sudo bash -c "cat >/var/www/html/relay.conf <<EOL
-TestingTorNetwork 1
-DataDirectory /var/lib/tor
-RunAsDaemon 1
-ConnLimit 60
-ShutdownWaitLength 0
-PidFile /var/lib/tor/pid
-Log notice file /var/log/tor/notice.log
-Log info file /var/log/tor/info.log
-Log debug file /var/log/tor/debug.log
-ProtocolWarnings 1
-SafeLogging 0
-DisableDebuggerAttachment 0
-DirAuthority $HOSTNAME orport=5000 no-v2 hs v3ident=$finger1 192.168.1.4:7000 $finger2
-SocksPort 0
-OrPort 5000
-ControlPort 9051
-# An exit policy that allows exiting to IPv4 LAN
-ExitPolicy accept 192.168.0.0/16:*
+sudo bash -c "cat >/var/www/html/fingerprint <<EOL
+DirAuthority $HOSTNAME orport=5000 no-v2 hs v3ident=$finger1 $ADDRESS:7000 $finger2
 EOL"
 
-sudo bash -c "cat >/var/www/html/client.conf <<EOL
-TestingTorNetwork 1
-DataDirectory /var/lib/tor
-RunAsDaemon 1
-ConnLimit 60
-ShutdownWaitLength 0
-PidFile /var/lib/tor/pid
-Log notice file /var/log/tor/notice.log
-Log info file /var/log/tor/info.log
-Log debug file /var/log/tor/debug.log
-ProtocolWarnings 1
-SafeLogging 0
-DisableDebuggerAttachment 0
-DirAuthority $HOSTNAME orport=5000 no-v2 hs v3ident=$finger1 192.168.1.4:7000 $finger2
-SocksPort 9050
-ControlPort 9051
-EOL"
+
+sleep 30
+
+for i in 1 2 3 4 5 6 7 8 9 10
+do
+   wget -qO- http://dir"$i"/fingerprint  | sudo tee -a /etc/tor/torrc
+done
+
 
 sudo /etc/init.d/tor start
 
