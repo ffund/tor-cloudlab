5ae0e2811a0d72b358fc87cd116f97882695ddb8
Fraida Fund <ffund@nyu.edu>
2024-12-03 09:55:45 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -18,20 +18,6 @@ sudo -u debian-tor nyx
 
 on any of the Tor hosts (client, relay, or directory server).
 
-To verify that the anonymity is working, on the client, run the 
-following to see how you appear to the web server:
-
-```
-wget -qO- http://10.10.253.200/
-```
-
-Then, run the same command with Tor, and see how you appear to 
-the webserver:
-
-```
-proxychains wget -qO- http://10.10.253.200/
-```
-
 If you need to restart the Tor process on any host:
 
 ```
@@ -70,58 +56,55 @@ request = pc.makeRequestRSpec()
 
 # Link between routers
 link_r = request.Link('link-r')
-link_r.Site('Site 1')
 
 # Link between webserver and router 3
 link_web = request.Link('link-web')
-link_web.Site('Site 1')
 
 # First, set up routers
 
 # Node router1
 node_router_1 = request.XenVM('router1')
 node_router_1.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD'
-node_router_1.Site('Site 1')
 iface_r1 = node_router_1.addInterface('interface-router1', pg.IPv4Address('10.10.254.1','255.255.255.0'))
 iface_r1.bandwidth = params.cap
 link_r.addInterface(iface_r1)
+node_router_1.exclusive = True
 
 # Node router2
 node_router_2 = request.XenVM('router2')
 node_router_2.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD'
-node_router_2.Site('Site 1')
 iface_r2 = node_router_2.addInterface('interface-router2', pg.IPv4Address('10.10.254.2','255.255.255.0'))
 iface_r2.bandwidth = params.cap
 link_r.addInterface(iface_r2)
+node_router_2.exclusive = True
 
 # Node router3
 node_router_3 = request.XenVM('router3')
 node_router_3.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD'
-node_router_3.Site('Site 1')
 iface_r3 = node_router_3.addInterface('interface-router3', pg.IPv4Address('10.10.254.3','255.255.255.0'))
 iface_r3.bandwidth = params.cap
 link_r.addInterface(iface_r3)
 iface_r3_web  = node_router_3.addInterface('interface-r3-web', pg.IPv4Address('10.10.253.1','255.255.255.0'))
 iface_r3_web.bandwidth = params.cap
 link_web.addInterface(iface_r3_web)
+node_router_3.exclusive = True
 
 
 # set up webserver
 node_webserver = request.XenVM('webserver')
 node_webserver.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD'
-node_webserver.Site('Site 1')
 node_webserver.ram = 4096
 node_webserver.addService(pg.Execute(shell="sh", command="/usr/bin/sudo /bin/bash /local/repository/webserver-install.sh"))
 iface_webserver = node_webserver.addInterface('interface-webserver', pg.IPv4Address('10.10.253.200','255.255.255.0'))
 iface_webserver.bandwidth = params.cap
 link_web.addInterface(iface_webserver)
+node_webserver.exclusive = True
 
 # set up clients!
 for i in range(params.n_client):
 	# Node client
 	node_client = request.XenVM('client' + str(i+1))
 	node_client.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD'
-	node_client.Site('Site 1')
 	node_client.ram = 4096
 	node_client.addService(pg.Execute(shell="sh", command="/usr/bin/sudo /bin/bash /local/repository/client-install.sh"))
 	iface_client = node_client.addInterface('interface-client-' + str(i+1), pg.IPv4Address('10.10.' + str(200+1+i) + '.' + str(i+1),'255.255.255.0'))
@@ -133,6 +116,7 @@ for i in range(params.n_client):
 	iface_router1 = node_router_1.addInterface('interface-r1-' + str(i+1), pg.IPv4Address('10.10.' + str(200+1+i) + '.254','255.255.255.0'))
 	iface_router1.bandwidth = params.cap
 	link_clients_router.addInterface(iface_router1)
+	node_client.exclusive = True
 
 
 # Node directoryserver
@@ -140,7 +124,6 @@ for i in range(params.n_dir):
 	# set up directory servers
 	node_dir = request.XenVM('dir' + str(i+1))
 	node_dir.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD'
-	node_dir.Site('Site 1')
 	node_dir.ram = 4096
 	node_dir.addService(pg.Execute(shell="sh", command="/usr/bin/sudo /bin/bash /local/repository/dir-install.sh"))
 	# Link for tor network
@@ -152,7 +135,8 @@ for i in range(params.n_dir):
 	iface_router2 = node_router_2.addInterface('interface-r2-dir' + str(i+1), pg.IPv4Address('10.10.' + str(i+1) + '.254','255.255.255.0'))
 	iface_router2.bandwidth = params.cap
 	link_tor.addInterface(iface_router2)
-
+	node_dir.exclusive = True
+	
 # set up relay nodes
 for i in range(params.n_relay):
 	node_relay = request.XenVM('relay' + str(i+1))
@@ -162,14 +146,13 @@ for i in range(params.n_relay):
 	node_relay.addService(pg.Execute(shell="sh", command="/usr/bin/sudo /bin/bash /local/repository/relay-install.sh"))
 	# Link for tor network
 	link_tor = request.Link('link-tor-' + str(i+1+100))
-	link_tor.Site('Site 1')
 	iface_relay = node_relay.addInterface('interface-relay' + str(i+1), pg.IPv4Address('10.10.' + str(i+1+100) + '.' + str(i+1),'255.255.255.0'))
 	iface_relay.bandwidth = params.cap
 	link_tor.addInterface(iface_relay)
 	iface_router2 = node_router_2.addInterface('interface-r2-relay' + str(i+1), pg.IPv4Address('10.10.' + str(i+1+100) + '.254','255.255.255.0'))
 	iface_router2.bandwidth = params.cap
 	link_tor.addInterface(iface_router2)
-
+	node_relay.exclusive = True
 
 # Print the generated rspec
 pc.printRequestRSpec(request)
